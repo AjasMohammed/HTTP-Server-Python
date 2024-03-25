b2d63806e6bf225388690f8bff3ac4c14c5c4598
Ajas Mohammed <ajasmohammed33@gmail.com>
2024-03-25 22:52:30 +0530

    Added support for Reciving and Sending data through TCP connection.

--- app/main.py
+++ app/main.py
@@ -4,10 +4,18 @@ import socket
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
+    host = '127.0.0.1'
+    port = 4221
+    response = b'HTTP/1.1 200 OK\r\n\r\n'
 
-    
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    with socket.create_server((host, port)) as server_socket:
+        client_socket, client_address = server_socket.accept()
+        print(f"Connection from {client_address} has been established.")
+
+        data = client_socket.recv(1024)
+        if data:
+            client_socket.sendall(response)
+        
 
 
 if __name__ == "__main__":
