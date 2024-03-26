e6e6c9e65f5718e127ceda6d2733b94fe049c77f
Ajas Mohammed <ajasmohammed33@gmail.com>
2024-03-26 14:40:34 +0530

    Accepts the directory argument from cli. If provided filename in the url exists in the directory, the server returns the file contents else 404.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-import socket, threading
+import socket, threading, argparse, os
 
 
 def create_response(status_code, content_length, content, content_type="text/plain"):
@@ -6,8 +6,15 @@ def create_response(status_code, content_length, content, content_type="text/pla
     return response
 
 
-def handle_request(client_socket):
+def check_file(directory, path):
+    print("Directory: ", directory, " Path: ", path)
+    path = os.path.join(directory, path)
+    return os.path.exists(path)
+
+
+def handle_request(client_socket, directory):
     response = b"HTTP/1.1 200 OK\r\n\r\n"
+    not_found_status = b"HTTP/1.1 404 Not Found\r\n\r\n"
     with client_socket:
         bytes_data = client_socket.recv(1024)
         if bytes_data:
@@ -22,9 +29,16 @@ def handle_request(client_socket):
             elif path.startswith("/user-agent"):
                 content = data[2].split(":")[1].strip()
                 response = create_response("200 OK", len(content), content)
-
+            elif path.startswith("/files/"):
+                filename = path.replace("/files/", "")
+                if check_file(directory, filename):
+                    with open(os.path.join(directory, filename), "rb") as file:
+                        content = file.read()
+                        response = create_response("200 OK", len(content), content, content_type='application/octet-stream')
+                else:
+                    response = not_found_status
             else:
-                response = b"HTTP/1.1 404 Not Found\r\n\r\n"
+                response = not_found_status
         client_socket.send(response)
 
 
@@ -33,11 +47,16 @@ def main():
     host = "127.0.0.1"
     port = 4221
 
+    parser = argparse.ArgumentParser(description="HTTP Server")
+    parser.add_argument("--directory", type=str, help="directory to serve", default=None)
+    args = parser.parse_args()  # parse commandline arguments
+    directory = args.directory
+
     with socket.create_server((host, port)) as server_socket:
         while True:
             client_socket, client_address = server_socket.accept()
             print(f"Connection from {client_address} has been established.")
-            connection = threading.Thread(target=handle_request, args=(client_socket,))
+            connection = threading.Thread(target=handle_request, args=(client_socket, directory))
             connection.start()
 
 
